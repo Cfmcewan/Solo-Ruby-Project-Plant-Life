c1c43e5f15758e0e860860a822c2fce4efefd8c0
Clare McEwan <cfmcewan@gmail.com>
2018-12-09 12:19:49 +0000

    new and edit controller functions and views added for plants

--- plant_life/controllers/nursery_controller.rb
+++ plant_life/controllers/nursery_controller.rb
@@ -6,8 +6,30 @@ get '/nurseries' do
   erb(:"nurseries/index")
 end
 
+get '/nurseries/new' do
+  @nurseries = Nursery.all
+  erb(:"nurseries/new")
+end
+
+post '/nurseries' do
+  Nursery.new(params).save
+  redirect to '/nurseries'
+end
+
 get '/nurseries/:id' do
   @nursery = Nursery.find(params['id'])
   @plants = @nursery.plants()
   erb(:"nurseries/show")
+end
+
+get '/nurseries/:id/edit' do
+  @nursery = Nursery.find(params[:id])
+  @plants = Plant.all()
+  erb(:edit)
+end
+
+post '/nurseries/:id' do
+  @nursery = Nursery.new(params)
+  @nursery.update()
+  redirect("/nurseries/#{params[:id]}")
 end
--- plant_life/controllers/plant_controller.rb
+++ plant_life/controllers/plant_controller.rb
@@ -6,7 +6,35 @@ get '/plants' do
   erb(:"plants/index")
 end
 
+get '/plants/new' do
+  @nurseries = Nursery.all
+  erb(:"plants/new")
+end
+
+post '/plants' do
+  Plant.new(params).save
+  redirect to '/plants'
+end
+
 get '/plants/:id' do
   @plant = Plant.find(params['id'])
   erb(:"plants/show")
+end
+
+get '/plants/:id/edit' do
+  @nurseries = Nursery.all
+  @plant = Plant.find(params['id'])
+  erb(:"plants/edit")
+end
+
+post '/plants/:id' do
+  plant = Plant.new(params)
+  plant.update
+  redirect to "/plants/#{params['id']}"
+end
+
+post '/plants/:id/delete' do
+  plant = Plant.find(params['id'])
+  plant.delete
+  redirect to '/plants'
 end 
