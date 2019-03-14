633ed6f771a2ca7efc1d238d5f4b2d822dcf9ed9
Clare McEwan <cfmcewan@gmail.com>
2019-03-14 09:48:07 +0000

    updates db credentials

--- plant_life/db/sql_runner.rb
+++ plant_life/db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: "d5n684f0ucpfer", host: "ec2-107-21-99-237.compute-1.amazonaws.com", port: 5432, user:"pzlyepwxmutvxj", password: "757cf697e857c08609eaa2f8e31f32e3f86bb8e2934b80237d7ebff3fa66afbf"})
+      db = PG.connect({dbname: "d2dbiijho3rthk", host: "ec2-184-73-153-64.compute-1.amazonaws.com", port: 5432, user:"axhqtkmtszkynf", password: "ea989a3e27f9888a2c0cf9f72eb88e6856fea6435e7529b9086335f3291fcfdb"})
       db.prepare("query", sql)
       results = db.exec_prepared("query", values)
     ensure
